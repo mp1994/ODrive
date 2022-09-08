ba09387ed1492b5959e307ac4c9274ccaa6a2b5d
Mattia <mattia.pesenti@polimi.it>
2022-09-08 21:53:11 +0200

    Partial merge: ascii_protocol implementation from branch 'ascii-usb-custom'

--- Firmware/communication/ascii_protocol.hpp
+++ Firmware/communication/ascii_protocol.hpp
@@ -14,6 +14,8 @@ public:
     void start();
 
 private:
+    void cmd_set_torque_get_feedback_0(char * pStr);
+    void cmd_set_torque_get_feedback_1(char * pStr);
     void cmd_set_position(char * pStr, bool use_checksum);
     void cmd_set_position_wl(char * pStr, bool use_checksum);
     void cmd_set_velocity(char * pStr, bool use_checksum);
@@ -30,6 +32,7 @@ private:
     void cmd_encoder(char * pStr, bool use_checksum);
 
     template<typename ... TArgs> void respond(bool include_checksum, const char * fmt, TArgs&& ... args);
+    void respond_byte(bool include_checksum, uint8_t* tx_data, size_t count);
     void process_line(fibre::cbufptr_t buffer);
     void on_write_finished(fibre::WriteResult result);
     void on_read_finished(fibre::ReadResult result);
@@ -41,6 +44,38 @@ private:
     bool read_active_ = true;
 
     fibre::BufferedStreamSink<512> sink_;
+
+    uint8_t tx_buf_[512]; // Byte array response buffer 
+
 };
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+enum ascii85_errs_e
+{
+    ascii85_err_out_buf_too_small = -255,
+    ascii85_err_in_buf_too_large,
+    ascii85_err_bad_decode_char,
+    ascii85_err_decode_overflow
+};
+
+int32_t encode_ascii85 (const uint8_t *inp, int32_t in_length, uint8_t *outp, int32_t out_max_length);
+
+int32_t decode_ascii85 (const uint8_t *inp, int32_t in_length, uint8_t *outp, int32_t out_max_length);
+
+int32_t ascii85_get_max_encoded_length (int32_t in_length);
+
+int32_t ascii85_get_max_decoded_length (int32_t in_length);
+
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif // __ASCII_PROTOCOL_HPP
